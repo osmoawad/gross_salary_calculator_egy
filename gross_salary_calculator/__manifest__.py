cfff21f445f3dc29f9df2f8a9c08f94216727981
Osama Moawad <154897635+osmoawad@users.noreply.github.com>
2025-06-30 22:43:24 +0300

    update

--- gross_salary_calculator/__manifest__.py
+++ gross_salary_calculator/__manifest__.py
@@ -1,15 +1,21 @@
 {
     "odoo_version": "18.0",
-    "name": "Gross Salary Calculator",
+    "name": "Gross Salary Calculator - Egyptian Payroll",
     "author": "Osama Moawad",
     "version": "1.3",
     "summary": "Calculate gross salary from net",
     "category": "Human Resources",
-    "depends": ["hr"],
+    "depends": ["hr", "hr_contract"],
     "data": [
         "views/hr_contract_views.xml"
     ],
+    "images": [
+        "static/description/icon.png",
+        "static/description/cover.png"
+    ],
     "installable": True,
     "application": False,
-    "license": "LGPL-3"
+    "license": "LGPL-3",
+    "price": 200.0,
+    "currency": "USD"
 }
